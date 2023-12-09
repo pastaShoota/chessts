62a30dfce3154692e15b39e7fedecbd598d39696
thierry chevallier <thierry.chevallier31@gmail.com>
2023-12-09 08:45:41 -1000

    parse fen string

--- src/fen/fen.utils.spec.ts
+++ src/fen/fen.utils.spec.ts
@@ -1,4 +1,5 @@
-import { generateEmptyBoard } from "./fen.utils";
+import { generateEmptyBoard, positionFromFen } from "./fen.utils";
+import { Piece } from "../model/definitions";
 
 describe('fen utils', ()=>{
     describe('generate empty board', () => {
@@ -10,6 +11,52 @@ describe('fen utils', ()=>{
 
             expect(result[63]).toHaveProperty('row', 8);
             expect(result[63]).toHaveProperty('file', 'H');
+            
+            expect(result[36]).toHaveProperty('row', 5);
+            expect(result[36]).toHaveProperty('file', 'E');
+        
+            expect(result[26]).toHaveProperty('row', 4);
+            expect(result[26]).toHaveProperty('file', 'C');
+        });
+    });
+    describe('position from fen', () => {
+        const fen = '8/8/8/4p1K1/2k1P3/8/8/8 b - - 0 1';
+        // black pawn e5 (=36) white king g5 (=38) black king c4 (=26) white pawn e4 (=28)
+
+        it('should give all piece places correctly', () => {
+            const board = positionFromFen(fen).board;
+            expect(board[36].occupant).toBeDefined();
+            expect(board[38].occupant).toBeDefined();
+            expect(board[26].occupant).toBeDefined();
+            expect(board[28].occupant).toBeDefined();
+            
+            let occupant = board[36].occupant as Piece;
+            expect(occupant.type).toBe('pawn');
+            expect(occupant.color).toBe('black');
+            
+            occupant = board[38].occupant as Piece;
+            expect(occupant.type).toBe('king');
+            expect(occupant.color).toBe('white');
+   
+            occupant = board[26].occupant as Piece;
+            expect(occupant.type).toBe('king');
+            expect(occupant.color).toBe('black');
+
+            occupant = board[28].occupant as Piece;
+            expect(occupant.type).toBe('pawn');
+            expect(occupant.color).toBe('white');
+        });
+
+        it('should give only the specified pieces', () => {
+            const emptySquares = positionFromFen(fen).board.filter((sq,ix) => ![36,38,26,28].includes(ix));
+            
+            expect(emptySquares.length).toBeTruthy();
+            emptySquares.forEach((emptySquare) => {
+                expect(emptySquare.occupant).toBeUndefined();
+            })
+        });
+        it('should give correct list of pieces from side to move', () => {
+            fail("not implemented");
         });
     });
 });
--- src/fen/fen.utils.ts
+++ src/fen/fen.utils.ts
@@ -1,12 +1,34 @@
-import { Board, FileNumber, Position, RowNumber, Square } from "../model/definitions";
+import { Board, FileNumber, Position, RowNumber, Square, Piece } from "../model/definitions";
 
-const dummySquare: Square = {file: "A", row: 1};
-const emptyBoard: Board = generateEmptyBoard();
+const dummySquare: Square = {file: 'A', row: 1};
+
+type fenPieceCode = 'p'|'n'|'b'|'r'|'q'|'k'|'P'|'N'|'B'|'R'|'Q'|'K';
+
+const fenPieceMap: ReadonlyMap<fenPieceCode,Piece> = new Map([
+    ['p', {color: 'black', type: 'pawn'}],
+    ['n', {color: 'black', type: 'knight'}],
+    ['b', {color: 'black', type: 'bishop'}],
+    ['r', {color: 'black', type: 'rook'}],
+    ['q', {color: 'black', type: 'queen'}],
+    ['k', {color: 'black', type: 'king'}],
+    ['P', {color: 'white', type: 'pawn'}],
+    ['N', {color: 'white', type: 'knight'}],
+    ['B', {color: 'white', type: 'bishop'}],
+    ['R', {color: 'white', type: 'rook'}],
+    ['Q', {color: 'white', type: 'queen'}],
+    ['K', {color: 'white', type: 'king'}],
+]);
 
 function ixToCoordinates(ix: number): [FileNumber, RowNumber] {
     const colNumber = String.fromCharCode('A'.charCodeAt(0) + ix % 8) as FileNumber;
     const rowNumber = (Math.floor(ix / 8) + 1) as RowNumber;
-    return [colNumber, rowNumber];
+    return [colNumber, rowNumber]
+}
+
+function ixToSquare(ix: number): Square {
+    let square = {...dummySquare};
+    [square.file, square.row] = ixToCoordinates(ix);
+    return square;
 }
 
 export function generateEmptyBoard(): Board {
@@ -17,10 +39,37 @@ export function generateEmptyBoard(): Board {
     }) as Board;
 }
 
+function pieceFromFen(pieceLetter: string): Piece|undefined {
+    return fenPieceMap.get(pieceLetter as fenPieceCode);
+}
+
+function generateEmptySquares(fromIx: number, length: number =1): Square[] {
+    if (fromIx < 0 || fromIx > 63 || length < 1 || length > 64) {
+        return [];
+    }
+    const indexes = Array.from(Array(length).keys()).map((x) => x + fromIx);
+    return indexes.map(ixToSquare);
+}
+
 export function positionFromFen(fen: string): Position {
-    // TODO
+    const [fenRows, turn, castling, ] = fen.split(" ");
+    let squaresBuilder: Square[] = [];
+    let squareCounter = 0;
+    fenRows.split('/').reverse().forEach((row) => {
+        row.split('').forEach((char) => {
+            if (Number(char)) {
+                squaresBuilder.push(...generateEmptySquares(squareCounter, Number(char)));
+                squareCounter += Number(char);
+            } else {
+                let square = generateEmptySquares(squareCounter)[0];
+                square.occupant = pieceFromFen(char);
+                squaresBuilder.push(square);
+                squareCounter++;
+            }
+        });
+    });
     return {
-        board: emptyBoard,
-        sideToMove: []
+        board: squaresBuilder as Board,
+        sideToMove: [] // TODO upgrade side to move pieces to movable
     };
 }
